4fe73e84841aeb6bf40bdf28aa930acf3b7fa93f
fireyy <fireyy@gmail.com>
2017-12-01 15:55:57 +0800

    feat: split rollup config

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -3,22 +3,27 @@ import commonjs from 'rollup-plugin-commonjs';<% if (buble) { %>
 import buble from 'rollup-plugin-buble';<% } %>
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'umd',
-      name: '<%= name %>'
-    },
-    {
-      file: pkg.module,
-      format: 'es'
-    }
-  ],
-  plugins: [<% if (buble) { %>
-    buble(),<% } %>
-    resolve(),
-    commonjs()
-  ]
-}
+export default [
+	{
+		input: 'src/index.js',
+		output: {
+			file: pkg.browser,
+			format: 'umd'
+		},
+		name: '<%= moduleName %>',
+		plugins: [<% if (buble) { %>
+      buble(),<% } %>
+			resolve(),
+			commonjs()
+		]
+	},
+	{
+		input: 'src/index.js',
+		external: [],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+    ],
+    plugins: [<% if (buble) { %>buble()<% } %>]
+	}
+]
